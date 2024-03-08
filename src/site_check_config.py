14ef3a9dc81e53d4163b06b751a1dec03c483aad
Travis <tmann1794@gmail.com>
2024-03-08 11:51:09 -0500

    Added logging retention period

--- src/site_check_config.py
+++ src/site_check_config.py
@@ -17,4 +17,5 @@ class SiteCheckConfig:
         self.email_password = config["email_password"]
         self.run_frequency_sec = config["run_frequency_sec"]
         self.max_check_attempts = config["max_check_attempts"]
+        self.log_retention_period_days = config["log_retention_period_days"]
         self.receivers = [self.sender]
