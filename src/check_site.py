76f0b30cf01ebcf7cea2d9c1fae7e0266438ecdf
Travis <tmann1794@gmail.com>
2024-03-10 13:23:12 -0400

    refactored os system calls into polymorphic implementations of OSSeleniumHandler

--- src/check_site.py
+++ src/check_site.py
@@ -1,105 +1,41 @@
-from selenium.webdriver import Chrome, ChromeOptions
-from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.common.exceptions import TimeoutException
-from pyvirtualdisplay import Display
 
-import os
 from typing import List
-import shutil
 import logging
 
+from os_selenium_handler import OSSeleniumHandler, get_os_selenium_handler
 from run_check import run_check, RunCheckTimeoutException
 from site_check_config import SiteCheckConfig
 
 
-# --- glob ---
-PROFILE_DATA_FOLDER = os.path.join(os.path.dirname(os.path.dirname(__file__)), "profile_data")
-
-
-# --- func ---
-def is_windows() -> bool:
-    return os.name == "nt"
-
-
-def get_driver() -> Chrome:
-    """
-    purpose: create selenium driver
-    :return:
-    """
-    # create virtual display for raspberry pi
-    if not is_windows():
-        logging.info("creating virtual display")
-        display = Display(visible=0, size=(800, 600))
-        display.start()
-
-    logging.info("creating chrome driver")
-    max_attempts = 3
-    for attempt in range(max_attempts):
-        # attempt to resolve environment issues
-        if attempt == 1:
-            kill_drivers()
-        elif attempt == 2:
-            delete_profile_data()
-
-        # create driver
-        try:
-            options = ChromeOptions()
-            options.add_argument(f"--user-data-dir={PROFILE_DATA_FOLDER}")
-            logging.info(f"os name: {os.name}")
-            if is_windows():  # selenium manager will find driver
-                return Chrome()
-            else:  # need to use custom driver on raspberry pi
-                return Chrome(service=Service(executable_path="/usr/lib/chromium-browser/chromedriver"))
-        except Exception as e:
-            logging.info(f"failed to create driver due to {type(e)}: {e}")
-            if attempt == max_attempts:
-                raise e
-
-
-def kill_drivers() -> None:
-    logging.warning("killing all driver processes")
-    if is_windows():
-        os.system("taskkill /F /IM chromedriver.exe")
-        os.system("taskkill /F /IM chrome.exe")
-    else:
-        os.system("pkill chromedriver")
-        os.system("pkill chrome")
-    logging.warning("processes killed")
-
-
-def delete_profile_data() -> None:
-    logging.warning("deleting profile data")
-    shutil.rmtree(PROFILE_DATA_FOLDER)
-    logging.warning("profile data deleted")
-
-
 def check_site(scf: SiteCheckConfig) -> List[str]:
-    result = []
+    result = ["ERROR: no result obtained from checks"]
+    os_selenium_handler = get_os_selenium_handler()
     for attempt in range(scf.max_check_attempts):
         logging.info(f"starting check_site attempt {attempt}")
         try:
             check_time_sec = int(scf.run_frequency_sec * 0.9 / scf.max_check_attempts)
-            result = run_check(check_site_single_attempt, check_time_sec, scf)
+            result = run_check(check_site_single_attempt, check_time_sec, os_selenium_handler, scf)
         except RunCheckTimeoutException:
-            logging.info(f"check_site attempt {attempt} timed out")
-            kill_drivers()
+            logging.warning(f"check_site attempt {attempt} timed out")
+            os_selenium_handler.kill_drivers()
             result = ["check exceeded timeout"]
         if not result:
             logging.info(f"check_site attempt {attempt} passed")
             return result
-        logging.info(f"check_site attempt {attempt} failed")
-    logging.info(f"all {scf.max_check_attempts} attempts for check_site exhausted, returning final result")
+        logging.warning(f"check_site attempt {attempt} failed")
+    logging.warning(f"all {scf.max_check_attempts} attempts for check_site exhausted, returning final result")
     return result
 
 
-def check_site_single_attempt(scf: SiteCheckConfig) -> List[str]:
+def check_site_single_attempt(os_selenium_handler: OSSeleniumHandler, scf: SiteCheckConfig) -> List[str]:
     """
     purpose: checks if a site is accessible
     :return:
     """
-    driver = get_driver()
+    driver = os_selenium_handler.get_chrome_driver()
     driver.implicitly_wait(20)
 
     logging.info(f"going to {scf.site}")
---
+++ src/os_selenium_handler.py
@@ -0,0 +1,106 @@
+import os
+import shutil
+import logging
+from typing import List
+from pyvirtualdisplay import Display
+from selenium.webdriver import Chrome, ChromeOptions
+from selenium.webdriver.chrome.service import Service
+
+
+PROFILE_DATA_FOLDER = os.path.join(os.path.dirname(os.path.dirname(__file__)), "profile_data")
+
+
+class OSSeleniumHandler:
+    @staticmethod
+    def is_this_os() -> bool:
+        raise NotImplementedError()
+
+    @staticmethod
+    def kill_drivers() -> None:
+        raise NotImplementedError()
+
+    @staticmethod
+    def get_chrome_driver() -> Chrome:
+        raise NotImplementedError()
+
+    @staticmethod
+    def delete_profile_data() -> None:
+        raise NotImplementedError()
+
+    @staticmethod
+    def get_chrome_driver_options() -> ChromeOptions:
+        options = ChromeOptions()
+        options.add_argument(f"--user-data-dir={PROFILE_DATA_FOLDER}")
+        options.add_argument('--disable-dev-shm-usage')
+        options.add_argument('--headless=new')
+        return options
+
+
+class LinuxSeleniumHander(OSSeleniumHandler):
+    _virtual_display_started = False
+
+    @staticmethod
+    def is_this_os() -> bool:
+        return os.name == "posix"
+
+    @staticmethod
+    def kill_drivers():
+        os.system("pkill chromedriver")
+        os.system("pkill chrome")
+
+    @staticmethod
+    def get_chrome_driver():
+        if not LinuxSeleniumHander._virtual_display_started:
+            LinuxSeleniumHander._start_virtual_display()
+        options = LinuxSeleniumHander.get_chrome_driver_options()
+        return Chrome(
+            options=options,
+            service=Service(executable_path="/usr/lib/chromium-browser/chromedriver")
+        )
+
+    @staticmethod
+    def delete_profile_data():
+        logging.warning("deleting profile data")
+        shutil.rmtree(PROFILE_DATA_FOLDER)
+        logging.warning("profile data deleted")
+
+    @staticmethod
+    def _start_virtual_display():
+        logging.info("creating virtual display")
+        display = Display(visible=0, size=(800, 600))
+        display.start()
+        logging.info("virtual display created")
+        LinuxSeleniumHander._virtual_display_started = True
+
+
+class WindowsSeleniumHandler(OSSeleniumHandler):
+    @staticmethod
+    def is_this_os() -> bool:
+        return os.name == "nt"
+
+    @staticmethod
+    def kill_drivers():
+        os.system("taskkill /F /IM chromedriver.exe")
+        os.system("taskkill /F /IM chrome.exe")
+
+    @staticmethod
+    def get_chrome_driver():
+        return Chrome(options=WindowsSeleniumHandler.get_chrome_driver_options())
+
+    @staticmethod
+    def delete_profile_data():
+        logging.warning("deleting profile data")
+        shutil.rmtree(PROFILE_DATA_FOLDER)
+        logging.warning("profile data deleted")
+
+
+class OSHandlerNotFoundException(Exception):
+    pass
+
+
+def get_os_selenium_handler():
+    handlers: List[OSSeleniumHandler] = [LinuxSeleniumHander, WindowsSeleniumHandler]
+    for handler in handlers:
+        if handler.is_this_os():
+            return handler
+    raise OSHandlerNotFoundException(f'no selenium handler found for "{os.name}"')
