4754bdc254f8128ba3d99c6ae2f1c74a4df7bc88
Travis <tmann1794@gmail.com>
2024-03-18 13:07:37 -0400

    Added retry to send_email

--- src/common/send_email.py
+++ src/common/send_email.py
@@ -1,10 +1,26 @@
 import smtplib
+import logging
+from time import sleep
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 
 
 ENDPOINT = 'smtp.gmail.com'
 PORT = 0
+
+
+def send_mail_attempt_with_retries(server: smtplib.SMTP, msg: MIMEMultipart) -> None:
+    max_attempts = 3
+    retry_wait_seconds_multiplier = 3
+
+    for attempt_idx in range(max_attempts):
+        try:
+            server.sendmail(msg.get('From'), msg["To"], msg.as_string())
+            return
+        except smtplib.SMTPDataError as e:
+            logging.error(f"Failed to send email due to {type(e)}: {e}")
+            sleep(attempt_idx * retry_wait_seconds_multiplier)
+    raise RuntimeError(f'failed to send "{msg["Subject"]}" after {max_attempts} attempt(s)')
 
 
 def send_email(
@@ -33,7 +49,8 @@ def send_email(
     msg["To"] = ", ".join(recipients)
     if isinstance(cc, list):
         msg["cc"] = ", ".join(cc)
+
     server = smtplib.SMTP(host=endpoint, port=port)
     server.starttls()
     server.login(sender, sender_password)
-    server.sendmail(msg.get('From'), msg["To"], msg.as_string())
+    send_mail_attempt_with_retries(server, msg)
